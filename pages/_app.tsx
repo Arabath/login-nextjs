ae1c00a5654b86b35baba87567fce0a6a56b1124
Arabath <axelhobrecht@hotmail.com>
2023-08-18 12:09:07 -0300

    styles fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { SWRConfig } from 'swr';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { lightTheme } from '../themes';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <SWRConfig 
+      value={{
+        fetcher: (resource, init) => fetch(resource, init).then(res => res.json())
+      }}
+    >
+          <ThemeProvider theme={ lightTheme}>
+              <CssBaseline />
+              <Component {...pageProps} />
+          </ThemeProvider>
+    </SWRConfig>
+  )
 }
+
+export default MyApp
